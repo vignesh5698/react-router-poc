21d365007ecec2afeb1560e9f7a85c2ad6e318fa
vignesh5698 <vigneshit13@gmail.com>
2021-05-20 10:53:32 +0530

    Readme files added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,85 +6,85 @@ const { TabPane } = Tabs;
 
 class Home extends Component {
 
-	buildLocation = (location, tabName) => {
-		const { match: { path } } = this.props;
+  buildLocation = (location, tabName) => {
+    const { match: { path } } = this.props;
 
-		return { ...location, pathname: `${path}/${tabName}` }
-	};
+    return { ...location, pathname: `${path}/${tabName}` }
+  };
 
-	tabs = [
-		{
-			title: (
-				<Link
-					to={location => ({ ...location, pathname: "/home/first" })}
-				>
-					First
-				</Link>
-			),
-			key: 'first',
-			body: <h1>Content of Tab Pane 1</h1>
-		},
-		{
-			title: (
-				<Link
-					to={location => ({ ...location, pathname: "/home/second" })}
-				>
-					Second
-				</Link>
-			),
-			key: 'second',
-			body: <h1>Content of Tab Pane 2</h1>
-		},
-		{
-			title: (
-				<Link
-					to={location => ({ ...location, pathname: "/home/third" })}
-				>
-					Third
-				</Link>
-			),
-			key: 'third',
-			body: <h1>Content of Tab Pane 3</h1>
-		},
-	]
+  tabs = [
+    {
+      title: (
+        <Link
+          to={location => ({ ...location, pathname: "/home/first" })}
+        >
+          First
+        </Link>
+      ),
+      key: 'first',
+      body: <h1>Content of Tab Pane 1</h1>
+    },
+    {
+      title: (
+        <Link
+          to={location => ({ ...location, pathname: "/home/second" })}
+        >
+          Second
+        </Link>
+      ),
+      key: 'second',
+      body: <h1>Content of Tab Pane 2</h1>
+    },
+    {
+      title: (
+        <Link
+          to={location => ({ ...location, pathname: "/home/third" })}
+        >
+          Third
+        </Link>
+      ),
+      key: 'third',
+      body: <h1>Content of Tab Pane 3</h1>
+    },
+  ]
 
-	renderTabPanes = () => {
-		return this.tabs.map(tab => {
-			return (
-				<TabPane tab={tab.title} key={tab.key}>
-					{tab.body}
-				</TabPane>
-			)
-		})
-	}
+  renderTabPanes = () => {
+    return this.tabs.map(tab => {
+      return (
+        <TabPane tab={tab.title} key={tab.key}>
+          {tab.body}
+        </TabPane>
+      )
+    })
+  }
 
-	getActiveKey = (location) => {
-		return location.pathname.split('/')[2];
-	}
+  getActiveKey = (location) => {
+    return location.pathname.split('/')[2];
+  }
 
-	render() {
+  render() {
 
-		return (
-			<div>
-				<Router>
-					<Route path="/home/:path" render={({location}) => {
-						const activeKey = this.getActiveKey(location);
-						console.log('Active key:', activeKey);
+    return (
+      <div>
+        <Router basename={process.env.PUBLIC_URL}>
+          <Route path="/home/:path" render={({location}) => {
+            const activeKey = this.getActiveKey(location);
+            console.log('Active key:', activeKey);
 
-						return (
-							<div>
-								<Tabs activeKey={activeKey} onChange={() => console.log('Tab Changed')}>
-									{this.renderTabPanes()}
-								</Tabs>
-							</div>
-						)
-					}}
-					/>
-					{/* <Redirect from="/home" to="/home/first" /> */}
-				</Router>
-			</div>
-		);
-	}
+            return (
+              <div>
+                <Tabs activeKey={activeKey} onChange={() => console.log('Tab Changed')}>
+                  {this.renderTabPanes()}
+                </Tabs>
+              </div>
+            )
+          }}
+          />
+          {/* <Redirect from="/home" to="/home/first" /> */}
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default Home;
